681d2060306380c57f8eef76c4d26ff6e748e8ce
Madalin Valceleanu <devmadalin@vmadalin.com>
2019-08-15 08:15:55 +0200

    test precommit catalina

--- scripts/git-hooks/pre-commit.sh
+++ scripts/git-hooks/pre-commit.sh
@@ -2,20 +2,20 @@
 echo "Running static analysis..."
 
 OUTPUT="/tmp/analysis-result"
-./gradlew detekt > ${OUTPUT}
+./gradlew app:detekt app:ktlint app:lintDebug --daemon > ${OUTPUT}
 EXIT_CODE=$?
 
 if [[ ${EXIT_CODE} -ne 0 ]]; then
     cat ${OUTPUT}
     rm ${OUTPUT}
     echo "*********************************************"
-    echo "            Static Analysis Failed 2          "
+    echo "            Static Analysis Failed           "
     echo "Please fix the above issues before committing"
     echo "*********************************************"
     exit ${EXIT_CODE}
 else
     echo "*********************************************"
-    echo "      Static analysis found no problems      "
+    echo "      Static analysis no problems found      "
     echo "*********************************************"
     rm ${OUTPUT}
 fi
