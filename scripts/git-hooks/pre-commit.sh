b8256fbace2784d88c7af1d58f92cf110a5e7388
Madalin Valceleanu <devmobile@vmadalin.com>
2019-11-04 10:41:32 +0100

    Fixed code style warnings

--- scripts/git-hooks/pre-commit.sh
+++ scripts/git-hooks/pre-commit.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 echo "Running static analysis..."
 
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+export JAVA_HOME
 
 OUTPUT="/tmp/analysis-result"
 ./gradlew detekt ktlint lintDevDebug spotlessCheck --daemon > ${OUTPUT}
 EXIT_CODE=$?
-if [[ ${EXIT_CODE} -ne 0 ]]; then
+if [ ${EXIT_CODE} -ne 0 ]; then
     cat ${OUTPUT}
     rm ${OUTPUT}
     echo "*********************************************"
