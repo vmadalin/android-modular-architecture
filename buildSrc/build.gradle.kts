e945078668b37a9fbf71d8e8c5346c6e46697ce8
Madalin Valceleanu <devmobile@vmadalin.com>
2020-01-22 20:02:19 +0100

    Add stable android gradle versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -35,7 +35,7 @@ kotlinDslPluginOptions {
 }
 
 object PluginsVersions {
-    const val GRADLE_ANDROID = "4.0.0-alpha09"
+    const val GRADLE_ANDROID = "3.5.3"
     const val GRADLE_VERSIONS = "0.27.0"
     const val KOTLIN = "1.3.61"
     const val NAVIGATION = "2.1.0"
